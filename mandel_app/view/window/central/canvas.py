73731c64bd517673360aec6f97839c6194b2b5a0
Robin Carter <68642507+RobinLeeCarter@users.noreply.github.com>
2020-08-20 12:22:56 +0100

    Make window/canvas variables protected like window_z/canvas

--- mandel_app/view/window/central/canvas.py
+++ mandel_app/view/window/central/canvas.py
@@ -22,23 +22,31 @@ class Canvas:
     def __init__(self):
         # was in learnpyqt tutorial and probably no harm but doesn't seem to do anything
         matplotlib.use('Qt5Agg')
-        self.mandel: Optional[mandelbrot.Mandel] = None
-        self.dpi = self.get_dpi()
-        self.fig: figure.Figure = figure.Figure(frameon=False, dpi=self.dpi)
+        self._mandel: Optional[mandelbrot.Mandel] = None
+        self._dpi = self.get_dpi()
+        self._fig: figure.Figure = figure.Figure(frameon=False, dpi=self._dpi)
         # Space around axes. Documentation not helpful. Taken from stack-overflow.
-        self.fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-        self.ax: figure.Axes = self.fig.subplots()
-        self.figure_canvas = XFigureCanvasQTAgg(self.fig)
-        self.ax_image: Optional[image.AxesImage] = None
+        self._fig.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        self._ax: figure.Axes = self._fig.subplots()
+        self._figure_canvas = XFigureCanvasQTAgg(self._fig)
+        self._ax_image: Optional[image.AxesImage] = None
         # self._z0_marker: lines.Line2D
         self._z0: Optional[complex] = None
         self._z0_marker = lines.Line2D([], [], marker='x', markersize=30, color="blue",
                                        zorder=1, visible=False)
-        self.connections = {}
+        self._connections = {}
+
+    @property
+    def mandel(self) -> Optional[mandelbrot.Mandel]:
+        return self._mandel
+
+    @property
+    def figure_canvas(self) -> backend_qt5agg.FigureCanvasQTAgg:
+        return self._figure_canvas
 
     @property
     def center_pixel_point(self) -> tuples.PixelPoint:
-        return tuples.PixelPoint(self.mandel.shape.x * 0.5, self.mandel.shape.y * 0.5)
+        return tuples.PixelPoint(self._mandel.shape.x * 0.5, self._mandel.shape.y * 0.5)
 
     def get_dpi(self):
         q_application = QtWidgets.QApplication.instance()   # get singleton
@@ -48,21 +56,21 @@ class Canvas:
         return dpi
 
     def draw_mandel(self, mandel_: mandelbrot.Mandel):
-        self.mandel = mandel_
+        self._mandel = mandel_
         # 7ms could potentially go faster with a lookup
-        transformed_iterations = 100*np.mod(np.log10(1+self.mandel.iteration), 1)
-        transformed_iterations[self.mandel.iteration == self.mandel.max_iteration] = 0
+        transformed_iterations = 100*np.mod(np.log10(1 + self._mandel.iteration), 1)
+        transformed_iterations[self._mandel.iteration == self._mandel.max_iteration] = 0
 
-        self.figure_canvas.resize(self.mandel.shape.x, self.mandel.shape.y)
-        self.ax.clear()
+        self.figure_canvas.resize(self._mandel.shape.x, self._mandel.shape.y)
+        self._ax.clear()
         # don't appear to do anything with fig.subplots_adjust set
-        self.ax.set_axis_off()
-        self.ax.margins(0, 0)
-        self.ax_image: image.AxesImage = self.ax.imshow(
+        self._ax.set_axis_off()
+        self._ax.margins(0, 0)
+        self._ax_image: image.AxesImage = self._ax.imshow(
             transformed_iterations,
             interpolation='none', origin='lower',
             cmap='hot', vmin=0, vmax=100, alpha=1.0, zorder=0)
-        self.ax.add_line(self._z0_marker)
+        self._ax.add_line(self._z0_marker)
         if self._z0 is not None:
             self._set_z0_marker()
 
@@ -74,7 +82,7 @@ class Canvas:
 
     def rotate_mandel_frame(self, to_theta_degrees: int):
         # new rotation is required rotation minus current rotation
-        theta_delta = to_theta_degrees - self.mandel.theta_degrees
+        theta_delta = to_theta_degrees - self._mandel.theta_degrees
         # to rotate the frame one direction we must rotate the image in the opposite direction
         degrees = -theta_delta
         self._rotate_mandel(degrees)
@@ -112,10 +120,10 @@ class Canvas:
         if transform is None:  # no transformation, just draw
             transform = transforms.Affine2D()
         # widget places image in top left. Our co-ordinate system is bottom right.
-        if self.mandel.offset != tuples.PixelPoint(0, 0):
-            transform = transform.translate(self.mandel.offset.x, -self.mandel.offset.y)
-        trans_data = transform + self.ax.transData
-        self.ax_image.set_transform(trans_data)
+        if self._mandel.offset != tuples.PixelPoint(0, 0):
+            transform = transform.translate(self._mandel.offset.x, -self._mandel.offset.y)
+        trans_data = transform + self._ax.transData
+        self._ax_image.set_transform(trans_data)
         self._z0_marker.set_transform(trans_data)
         # self.ax.add_line(self._z0_marker)
         # self.ax.plot([0.1], [0.1], marker='x', markersize=10, color="blue", zorder=10)
@@ -129,7 +137,7 @@ class Canvas:
     def _set_z0_marker(self):
         pixel_x: List[int] = []
         pixel_y: List[int] = []
-        pixel_point: Optional[tuples.PixelPoint] = self.mandel.get_pixel_from_complex(self._z0)
+        pixel_point: Optional[tuples.PixelPoint] = self._mandel.get_pixel_from_complex(self._z0)
         if pixel_point is not None:
             pixel_x.append(pixel_point.x)
             pixel_y.append(pixel_point.y)
@@ -143,16 +151,16 @@ class Canvas:
         self.figure_canvas.draw()
 
     def save(self, file_path: str):
-        self.fig.savefig(IMAGE_PATH + file_path, format="png",
-                         pad_inches=0, bbox_inches='tight', transparent=True, dpi=self.dpi)
+        self._fig.savefig(IMAGE_PATH + file_path, format="png",
+                          pad_inches=0, bbox_inches='tight', transparent=True, dpi=self._dpi)
 
     #     """See: https://matplotlib.org/3.1.1/users/event_handling.html"""
     def add_connection(self, event_name: str, func: Callable[[backend_bases.Event], None]):
         connection_id = self.figure_canvas.mpl_connect(event_name, func)
-        self.connections[event_name] = connection_id
+        self._connections[event_name] = connection_id
 
     def remove_connection(self, event_name: str):
-        connection_id = self.connections[event_name]
+        connection_id = self._connections[event_name]
         self.figure_canvas.mpl_disconnect(connection_id)
 
     def set_cursor(self, cursor_shape: Qt.CursorShape):
@@ -160,21 +168,21 @@ class Canvas:
         self.figure_canvas.setCursor(cursor)
 
     def on_resized(self, new_image_space: tuples.ImageShape):
-        image_shape = self.mandel.shape
+        image_shape = self._mandel.shape
         x_offset = min(int((new_image_space.x - image_shape.x) / 2.0), 0)
         y_offset = min(int((new_image_space.y - image_shape.y) / 2.0), 0)
-        self.mandel.offset = tuples.PixelPoint(x=x_offset, y=y_offset)
+        self._mandel.offset = tuples.PixelPoint(x=x_offset, y=y_offset)
         self._transform_and_draw()
 
     # with the image offset, event gives an odd x, y position.
     # this function converts it back to a sensible one
     def get_image_point(self, event: backend_bases.MouseEvent):
-        image_point = tuples.PixelPoint(x=event.x - self.mandel.offset.x,
-                                        y=event.y + self.mandel.offset.y)
+        image_point = tuples.PixelPoint(x=event.x - self._mandel.offset.x,
+                                        y=event.y + self._mandel.offset.y)
         return image_point
 
     def above_center(self, y: int) -> bool:
-        return y >= self.mandel.shape.y / 2
+        return y >= self._mandel.shape.y / 2
 
 
 class XFigureCanvasQTAgg(backend_qt5agg.FigureCanvasQTAgg):
